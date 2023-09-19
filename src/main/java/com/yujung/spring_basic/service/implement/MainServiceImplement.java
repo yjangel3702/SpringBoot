d807a5fe142482b13ff0ba65b4c42e48042a57c6
kimyujung <yjangel3702@gmail.com>
2023-09-19 09:54:08 +0900

    어려워

--- src/main/java/com/yujung/spring_basic/service/implement/MainServiceImplement.java
+++ src/main/java/com/yujung/spring_basic/service/implement/MainServiceImplement.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.yujung.spring_basic.dto.request.PostUserRequestDto;
 import com.yujung.spring_basic.dto.response.PostUserResponseDto;
+import com.yujung.spring_basic.dto.response.ResponseDto;
 import com.yujung.spring_basic.entity.UserEntity;
 import com.yujung.spring_basic.repository.UserRepository;
 import com.yujung.spring_basic.service.MainService;
@@ -31,11 +32,17 @@ public class MainServiceImplement implements MainService {
     // INSERT INTO user (email, password, nickname, tell_number, address, address_detail)
     // VALUES(dto.getEmail(), dto.getPassword(), ...);
 
+    try {
     // description: Create 작업 순서 (INSERT) //
     // description: 1. Entity 인스턴스 생성 //
     UserEntity userEntity = new UserEntity(dto);
     // description: 2. repository의 save 메서드 사용 //
     userRepository.save(userEntity);
+    } catch (Exception exception) {
+      exception.printStackTrace();
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto("DBE", "Database Error"));
+    }
+    
 
     return ResponseEntity.status(HttpStatus.OK).body(new PostUserResponseDto("SU", "Success"));
 
