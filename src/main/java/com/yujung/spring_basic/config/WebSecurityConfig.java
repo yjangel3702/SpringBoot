9347d8181cfeab0da9ecc9c44f0be84a4235580f
kimyujung <yjangel3702@gmail.com>
2023-09-21 09:51:06 +0900

    진짜 넘 피곤해요

--- src/main/java/com/yujung/spring_basic/config/WebSecurityConfig.java
+++ src/main/java/com/yujung/spring_basic/config/WebSecurityConfig.java
@@ -1,11 +1,19 @@
 package com.yujung.spring_basic.config;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -47,11 +55,32 @@ public class WebSecurityConfig {
       // description: http method + url 패턴에 따른 지정 (POST /board 로 시작하는 모든 요청에 대하여 허용)
       .antMatchers(HttpMethod.POST, "/board/**").permitAll()
       // description: 나머지 모든 요청에 대하여 인증을 수행 //
-      .anyRequest().authenticated();
+      .anyRequest().authenticated().and()
+      // description: 인증 과정에서 발생한 예외를 직접 처리 //
+      .exceptionHandling().authenticationEntryPoint(new FailedAuthenticationEntryPoint());
 
     httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return httpSecurity.build();
   }
   
+}
+
+// description: 인증 과정에서 발생하는 예외에 대한 Response를 직접 처리하는 클래스 //
+// description: AuthenticationEntryPoint 인터페이스 구현 //
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+      throws IOException, ServletException {
+    
+    // description: Response의 컨텐츠 타입을 json으로 지정 //
+    response.setContentType("application/json");
+    // description: Response의 Http 상태 코드를 지정 //
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    // description: Response의 Body를 작성 //
+    response.getWriter().write("{ \"code\": \"AF\", \"message\": \"Authorization Failed\" }");
+                              //  { "code": "AF", "message": "Authorization Failed" }
+  }
+
 }
